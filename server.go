8e17465e587f226bd627f3794f5f35177ae5613d
John Roman <jrrmod@gmail.com>
2017-05-22 21:23:43 -0400

    jrr- write basic function to read incoming data.

--- server.go
+++ server.go
@@ -1,22 +1,71 @@
 package main
 
+import "os"
 import "net"
 import "fmt"
+import "time"
+import "bufio"
+import "sync"
 
-func handleConn(conn net.Conn) {
-	fmt.Println("host connected")
+type Session struct {
+	sid		int
+	connection	net.Conn
+	reader		*bufio.Reader
+	writer		*bufio.Writer
+	indata		chan string
+	outdata		chan string
+	sessionMutex	sync.Mutex
+}
+
+func NewSession(sid int, conn net.Conn) *Session {
+	reader := *bufio.NewReader(conn)
+	writer := *bufio.NewWriter(conn)
+
+	session := &Session {
+		sid: sid,
+		connection: conn,
+		reader: reader,
+		writer: writer,
+		indata: make(chan string)
+		outdata: make(chan string)
+	}
+	fmt.Printf("New Host connected, session id: %d\n ", sid)
+	return session
+}
+
+func (s *Session) Read() {
+	for {
+		str, err := s.reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Read string error")
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		s.indata <- str
+	}
+}
+
+func (s *Session) Write() {
+	for {
+	}
 }
 
 func main() {
-	ln, err := net.Listen("tcp", ":10000")
-	if err != nil {
-		fmt.Printf("%v\n", err.Error())
+	var port string = "3490"
+	servListener, listenErr := net.Listen("tcp", ":" + port)
+	if listenErr != nil {
+		fmt.Println("Listen server error")
+		os.Exit(1)
 	}
 
+	fmt.Println("Server listening on port " + port)
 	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			fmt.Printf("%v\n", err.Error())
+		conn, connErr := servListener.Accept()
+		if connErr != nil {
+			fmt.Println("Error connecting to server")
+			conn.Close()
+			time.Sleep(100 * time.Millisecond)
+			continue
 		}
 
 		go handleConn(conn)
